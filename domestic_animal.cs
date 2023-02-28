ecb23d5df5d99796ebea6f5a6a2fb2f35b28cb02
katerynafomina <kf30766@gmail.com>
2023-02-28 11:10:35 +0200

    add file class

---
+++ domestic_animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domestic_animal_practice2
+{
+    internal class domestic_animal
+    {
+
+    }
+}
