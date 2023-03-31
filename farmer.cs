35cd2bd570f78079f88bd2616c6aa941c6573e1d
katerynafomina <kf30766@gmail.com>
2023-03-31 20:01:03 +0300

    add class Farmer

---
+++ farmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domesticAnimal
+{
+    public class Farmer
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Farmer(string name)
+        {
+            this.name = name;
+        }
+        public void FarmerWithAnimal(object sender, EventArgs e)
+        {
+            Animal animal = sender as Animal;
+            Console.WriteLine("Фермер {0} повідомляє, що тварина {1} досягла продуктивного віку!", name, animal.Type);
+        }
+    }
+}
